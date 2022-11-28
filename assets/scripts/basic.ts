a00ff74e141ed48eed2191d089fcb33c8ed2b356
santy-wang <wh19930829@qq.com>
2022-11-28 19:37:28 +0800

    change texture

--- assets/scripts/basic.ts
+++ assets/scripts/basic.ts
@@ -34,54 +34,56 @@ export class basic extends Component {
     @property(Texture2D)
     backgroundTexture: Texture2D | null = null;
 
+    @property(Texture2D)
+    buttonTexture: Texture2D | null = null;
+
 
     start() {
         const document = new UIDocument();
         UISystem.instance.addDocument(document);
         const canvas = new UICanvas();
         const canvasLayout = document.window.addChild(canvas);
-        canvasLayout.horizontalAlignment = HorizontalAlignment.STRETCH;
-        canvasLayout.verticalAlignment = VerticalAlignment.STRETCH;
+        canvasLayout.horizontalAlignment = HorizontalAlignment.CENTER;
+        canvasLayout.verticalAlignment = VerticalAlignment.CENTER;
 
         const background = new Image();
         background.source = new UIBrush(this.backgroundTexture);
         const backgroundLayout = canvas.addChild(background);
         backgroundLayout.anchors = Anchors.BOTH_STRETCH;
-        backgroundLayout.offsets = new Thickness;
         backgroundLayout.pivot = new Vec2(0.5, 0.5);
 
         const leftTopImage = new Image();
         leftTopImage.source = new UIBrush(this.leftTopTexture);
         const leftTopImageLayout = canvas.addChild(leftTopImage);
         leftTopImageLayout.anchors = Anchors.TOP_LEFT;
-        leftTopImageLayout.offsets = new Thickness(0, 0, this.leftTopTexture.width / 2,this.leftTopTexture.height / 2);
+        leftTopImageLayout.useAutoSize = true;
 
         const topImage = new Image();
         topImage.source = new UIBrush(this.topTexture);
         const topImageLayout = canvas.addChild(topImage);
         topImageLayout.anchors = Anchors.TOP_CENTER;
-        topImageLayout.offsets = new Thickness(0, 0, this.topTexture.width / 2, this.topTexture.height / 2);
+        topImageLayout.useAutoSize = true;
         topImageLayout.pivot = new Vec2(0.5, 0);
 
         const rightTopImage = new Image();
         rightTopImage.source = new UIBrush(this.rightTopTexture);
         const rightTopImageLayout = canvas.addChild(rightTopImage);
         rightTopImageLayout.anchors = Anchors.TOP_RIGHT;
-        rightTopImageLayout.offsets = new Thickness(0, 0, this.rightTopTexture.width / 2, this.rightTopTexture.height / 2);
+        rightTopImageLayout.useAutoSize = true;
         rightTopImageLayout.pivot = new Vec2(1, 0);
 
         const rightImage = new Image();
         rightImage.source = new UIBrush(this.rightTexture);
         const rightImageLayout = canvas.addChild(rightImage);
         rightImageLayout.anchors = Anchors.CENTER_RIGHT;
-        rightImageLayout.offsets = new Thickness(0, 0, this.rightTexture.width / 2, this.rightTexture.height / 2);
+        rightImageLayout.useAutoSize = true;
         rightImageLayout.pivot = new Vec2(1, 0.5);
 
         const rightBottomImage = new Image();
         rightBottomImage.source = new UIBrush(this.rightBottomTexture);
         const rightBottomImageLayout = canvas.addChild(rightBottomImage);
         rightBottomImageLayout.anchors = Anchors.BOTTOM_RIGHT;
-        rightBottomImageLayout.offsets = new Thickness(0, 0, this.rightBottomTexture.width / 2, this.rightBottomTexture.height / 2);
+        rightBottomImageLayout.useAutoSize = true;
         rightBottomImageLayout.pivot = new Vec2(1, 1);
 
 
@@ -89,28 +91,28 @@ export class basic extends Component {
         centerBottomImage.source = new UIBrush(this.bottomTexture);
         const centerBottomImageLayout = canvas.addChild(centerBottomImage);
         centerBottomImageLayout.anchors = Anchors.BOTTOM_CENTER;
-        centerBottomImageLayout.offsets = new Thickness(0, 0, this.bottomTexture.width / 2, this.bottomTexture.height / 2);
+        centerBottomImageLayout.useAutoSize = true;
         centerBottomImageLayout.pivot = new Vec2(0.5, 1);
 
         const leftBottomImage = new Image();
         leftBottomImage.source = new UIBrush(this.leftBottomTexture);
         const leftBottomImageLayout = canvas.addChild(leftBottomImage);
         leftBottomImageLayout.anchors = Anchors.BOTTOM_LEFT;
-        leftBottomImageLayout.offsets = new Thickness(0, 0, this.leftBottomTexture.width / 2, this.leftBottomTexture.height / 2);
+        leftBottomImageLayout.useAutoSize = true;
         leftBottomImageLayout.pivot = new Vec2(0, 1);
 
         const leftImage = new Image();
         leftImage.source = new UIBrush(this.leftTexture);
         const leftImageLayout = canvas.addChild(leftImage);
         leftImageLayout.anchors = Anchors.CENTER_LEFT;
-        leftImageLayout.offsets = new Thickness(0, 0, this.leftTexture.width / 2, this.leftTexture.height / 2);
+        leftImageLayout.useAutoSize = true;
         leftImageLayout.pivot = new Vec2(0, 0.5);
 
         const centerImage = new Image();
         centerImage.source = new UIBrush(this.centerTexture);
         const centerImageLayout = canvas.addChild(centerImage);
         centerImageLayout.anchors = Anchors.CENTER_CENTER;
-        centerImageLayout.offsets = new Thickness(0, 0, this.centerTexture.width, this.centerTexture.height);
+        centerImageLayout.useAutoSize = true;
         centerImageLayout.pivot = new Vec2(0.5, 0.5);
     }
 
